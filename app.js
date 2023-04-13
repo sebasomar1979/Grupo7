59b98f3288c4c8c5126b215cb635bb7825e3c0c6
Sebastian Mussi <105326259+sebasomar1979@users.noreply.github.com>
2023-04-13 02:01:47 -0300

    actualizacion de cuadro registro

---
+++ app.js
@@ -0,0 +1,8 @@
+const nombre = document.getElementById("name")
+const apellido = document.getElementById("apellido")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+
+
+
+
